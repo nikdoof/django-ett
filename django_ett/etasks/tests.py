2dca6210af6ce972545c7e6b4b951ae1e7d003ad
Andrew Williams <andy@tensixtyone.com>
2012-10-19 23:00:27 +0100

    Added basic view test

--- django_ett/etasks/tests.py
+++ django_ett/etasks/tests.py
@@ -1,12 +1,12 @@
 from __future__ import with_statement
-from django.utils import unittest
+from django.test import TestCase
 from django.db import IntegrityError
 from django.contrib.auth.models import User
 
 from .models import Task
 
 
-class TaskTest(unittest.TestCase):
+class TaskTest(TestCase):
 
     def setUp(self):
         self.user, created = User.objects.get_or_create(username='TestUser')
@@ -14,7 +14,21 @@ class TaskTest(unittest.TestCase):
 
     def testDisplay(self):
         self.assertEqual(str(self.task), "Test Task 1")
+        self.task.name = ''
+        self.assertEqual(str(self.task), "Unknown")
 
     def testBadCreation(self):
         with self.assertRaises(IntegrityError):
             Task.objects.create(name='Bad Test')
+
+
+class TaskDetailViewTest(TestCase):
+
+    def setUp(self):
+        self.user, created = User.objects.get_or_create(username='TestUser')
+        self.task = Task.objects.create(user=self.user, name="Test Task 2")        
+
+    def testBasicResponse(self):
+        resp = self.client.get('/task/%d/' % self.task.pk)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('object' in resp.context)

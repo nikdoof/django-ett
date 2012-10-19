53fc8e5ed8a0c0a1b44bc0e9ce809be8be550645
Andrew Williams <andy@tensixtyone.com>
2012-10-19 22:27:04 +0100

    Added Jenkins testing support

---
+++ django_ett/django_ett/settings/test.py
@@ -0,0 +1,20 @@
+from .common import *
+
+INSTALLED_APPS += (
+    'django_jenkins',
+)
+
+PROJECT_APPS = [
+    'etasks',
+]
+
+JENKINS_TASKS = (
+    'django_jenkins.tasks.run_pylint',
+    'django_jenkins.tasks.with_coverage',
+    'django_jenkins.tasks.django_tests',
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_pyflakes',
+)
+
+
+print INSTALLED_APPS
